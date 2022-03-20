da47361729f1c8886c77582bb89ab5bed47257db
xiaofei_global <xiaofei_global@163.com>
2022-03-20 13:26:10 +0800

    测试主分支提交 4.1

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -41,7 +41,7 @@ public class App
         System.out.println("fork 提交2 ");
     }
     private void printMaster(){
-        System.out.println("master fdsf 支");
+        System.out.println("ma ster fdsf 支");
     }
 
 }
