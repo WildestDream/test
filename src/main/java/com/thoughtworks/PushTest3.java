e58b6c0b3c9454eacfc32f8598f9ed3c493d5ec7
xiaofei_global <xiaofei_global@163.com>
2022-03-30 20:34:58 +0800

    s3All

--- src/main/java/com/thoughtworks/PushTest3.java
+++ src/main/java/com/thoughtworks/PushTest3.java
@@ -3,5 +3,7 @@ package com.thoughtworks;
 public class PushTest3 {
     public static void main(String[] args) {
         System.out.println("squash test!2");
+        System.out.println("squash test!2");
+        System.out.println("squash test!2");
     }
 }
