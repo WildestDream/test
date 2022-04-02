b7fb5ca2fd35a3cefeadf2141e134c61a2cebda7
xiaofei_global <xiaofei_global@163.com>
2022-04-02 10:42:08 +0800

    t4

--- src/main/java/com/thoughtworks/PushTest3.java
+++ src/main/java/com/thoughtworks/PushTest3.java
@@ -9,6 +9,6 @@ public class PushTest3 {
         System.out.println("squash test!2");
         System.out.println("squash test!2");
         System.out.println("squash test!2");
-        System.out.println("squash test!2");
+        System.out.println("squash test!2ddwe");
     }
 }
